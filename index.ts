7b60a7155b2d346c482b4bfd4c29aca677cee692
Sam Cook <scook@esri.com>
2023-03-06 16:53:22 -0600

    removing OG tsx-dom

---
+++ index.ts
@@ -0,0 +1,37 @@
+import { IntrinsicElementsHTML, IntrinsicElementsSVG } from "./src/dom/types";
+
+export * from "./src/dom/createElement";
+export * from "./src/dom/jsx-runtime";
+export * from "./src/dom/types";
+
+export interface TsxConfig {
+    [s: string]: boolean;
+}
+
+// Returns TIF if T is specified as true in TsxConfig, otherwise TELSE
+type IfTsxConfig<T extends string, TIF, TELSE> = TsxConfig[T] extends false ? TELSE : TIF;
+
+type IntrinsicElementsCombined = IfTsxConfig<"html", IntrinsicElementsHTML, unknown> &
+    IfTsxConfig<"svg", IntrinsicElementsSVG, unknown>;
+
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace JSX {
+        // Return type of jsx syntax
+        type Element = IfTsxConfig<"html", HTMLElement, never> | IfTsxConfig<"svg", SVGElement, never>;
+
+        // The property name to use
+        interface ElementAttributesProperty {
+            props: unknown;
+        }
+
+        // The children name to use
+        interface ElementChildrenAttribute {
+            children: unknown;
+        }
+
+        // The available string tags
+        // eslint-disable-next-line @typescript-eslint/no-empty-interface
+        interface IntrinsicElements extends IntrinsicElementsCombined {}
+    }
+}
