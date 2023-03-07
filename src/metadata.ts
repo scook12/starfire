582c7facc3acf84d93016fdd3d264e64806f2e7b
Sam Cook <scook@esri.com>
2023-03-07 10:33:35 -0600

    metadata cleanliness refactor

--- src/metadata.ts
+++ src/metadata.ts
@@ -3,10 +3,12 @@ import { IComponent } from "./component"
 import { TIMELINE } from '@starbeam/universal';
 import render from './jsx';
 
+
 enum StarfireMetadata {
   tracked = "__starfire_tracked",
   fallback = "__starfire_fallback"
 }
+
 
 function setMetadata(metadata: string, target: Object, propertyKey?: PropertyKey): void {
   if (Reflect.hasMetadata(metadata, target))
@@ -19,12 +21,13 @@ export function track(target: Object, propertyKey?:PropertyKey): void {
   setMetadata(StarfireMetadata.tracked, target, propertyKey)
 }
 
+
 export function handleTracking(component: IComponent, renderFunc: () => void) {
-  if (Reflect.hasMetadata('__starfire_tracked', component)) {
-    Reflect.getMetadata('__starfire_tracked', component).forEach((trackedProp: string) => {
-      TIMELINE.on.change(Object.getOwnPropertyDescriptor(component, trackedProp)?.value, renderFunc)
-    })
-  }
+  if (!Reflect.hasMetadata(StarfireMetadata.tracked, component)) return
+  Reflect.getMetadata(StarfireMetadata.tracked, component).forEach((trackedProp: string) => {
+    const trackedValue = Object.getOwnPropertyDescriptor(component, trackedProp)?.value
+    TIMELINE.on.change(trackedValue, renderFunc)
+  })
 }
 
 
@@ -34,24 +37,20 @@ export function fallback(target: Object, propertyKey?: PropertyKey): void {
 
 
 export async function renderFallback(component: any): Promise<HTMLElement | null> {
-  let fallback
-  if (Reflect.hasMetadata('__starfire_fallback', component)) {
-    Reflect.getMetadata('__starfire_fallback', component).forEach((fallbackComponent: string)=> {
-      fallback = Object.getOwnPropertyDescriptor(component, fallbackComponent)?.value
-    })
-    if (fallback) {
-      render(fallback)
-      return await observeDomNodeInsertion(component.id)
-    }
-  }
-  return Promise.resolve(null)
+  if (!Reflect.hasMetadata(StarfireMetadata.fallback, component)) return Promise.resolve(null)
+  const propKey = Reflect.getMetadata(StarfireMetadata.fallback, component)[0]
+  const fallback = Object.getOwnPropertyDescriptor(component, propKey)?.value
+  render(fallback)
+  return await observeDomNodeInsertion(component.id)
 }
 
 
-export function observeDomNodeInsertion(id: string): Promise<HTMLElement | null> {
-  return new Promise(resolve => {
+function observeDomNodeInsertion(id: string): Promise<HTMLElement | null> {
+  return new Promise((resolve, reject) => {
+    const timeout = setTimeout(() => reject(new Error('DOM node never inserted')), 20000)
     const observer = new MutationObserver(muts => {
       if (document.getElementById(id)) {
+        clearTimeout(timeout)
         resolve(document.getElementById(id))
         observer.disconnect()
       }
