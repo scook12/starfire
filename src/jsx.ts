934b703fa36528cea172001e5ed1ce16f93c569d
Sam Cook <scook@esri.com>
2023-03-07 10:48:03 -0600

    house cleaning

--- src/jsx.ts
+++ src/jsx.ts
@@ -1,5 +1,5 @@
 import { handleTracking, renderFallback } from "./metadata";
-import { IComponent } from "./component"
+
 
 function getRootElement() {
   // todo make this configurable?
@@ -9,17 +9,16 @@ function getRootElement() {
 
 async function renderElement(component: any, parentNode?: JSX.Element) {
   await renderFallback(component)
-  const ElementFactory = async (component: IComponent) => await component.render()
+  const ElementFactory = async () => component.render()
   const node = document.getElementById(component.id)
-  const child = await ElementFactory(component)
+  const child = await ElementFactory()
   if (!node) {
     if (parentNode) parentNode.appendChild(child)
     else getRootElement()?.appendChild(child)
-  } else {
-    node.replaceWith(child)
-  }
+  } else node.replaceWith(child)
   return child
 }
+
 
 export default function render(component: any, parentNode?: JSX.Element): void {
   handleTracking(component, () => renderElement(component, parentNode))
