074c1badc72df66fe58feb00110daa8f98db550f
Sam Cook <scook@esri.com>
2023-03-07 12:14:39 -0600

    fix component type for async

--- src/component.ts
+++ src/component.ts
@@ -4,5 +4,5 @@ export interface IComponent {
   id: string;
   props?: unknown
   children?: JSX.ElementChildrenAttribute
-  render(): JSX.Element;
+  render(): JSX.Element | Promise<JSX.Element>;
 }
