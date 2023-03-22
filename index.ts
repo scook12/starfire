598de92612f7251f078236975674f611edbbed80
Sam Cook <scook@esri.com>
2023-03-22 09:44:55 -0500

    paths and cleanup

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import { IntrinsicElementsHTML, IntrinsicElementsSVG } from "./src/dom/types";
+import { IntrinsicElementsHTML, IntrinsicElementsSVG } from "starfire/dom/types";
 
-export * from "./src/dom/createElement";
-export * from "./src/dom/jsx-runtime";
-export * from "./src/dom/types";
+export * from "starfire/dom/createElement";
+export * from "starfire/dom/jsx-runtime";
+export * from "starfire/dom/types";
 
 export interface TsxConfig {
     [s: string]: boolean;
